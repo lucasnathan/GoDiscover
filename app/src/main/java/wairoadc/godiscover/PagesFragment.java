05e551f0123e26ac873d4d0e7c8e31636e561546
Nathan (yudi) <lucas.nathan80@hotmail.com>
2015-01-14 16:13:34 +1300

    14_01_2015_16_13

--- app/src/main/java/wairoadc/godiscover/PagesFragment.java
+++ app/src/main/java/wairoadc/godiscover/PagesFragment.java
@@ -1,8 +1,5 @@
 package wairoadc.godiscover;
 
-/**
- * Created by Lucas on 13/01/2015.
- */
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,15 +7,15 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class PagesFragment extends Fragment {
-
-    public PagesFragment(){}
-
-    @Override
+	
+	public PagesFragment(){}
+	
+	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
+            Bundle savedInstanceState) {
+ 
         View rootView = inflater.inflate(R.layout.fragment_pages, container, false);
-
+         
         return rootView;
     }
 }
--- app/src/main/java/wairoadc/godiscover/PhotosFragment.java
+++ app/src/main/java/wairoadc/godiscover/PhotosFragment.java
@@ -6,19 +6,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * Created by Lucas on 13/01/2015.
- */
+
 public class PhotosFragment extends Fragment {
-
-    public PhotosFragment() {
-    }
-
+	
+	public PhotosFragment(){}
+	
+	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-        View rootView = inflater.inflate(R.layout.fragment_pages, container, false);
-
+            Bundle savedInstanceState) {
+ 
+        View rootView = inflater.inflate(R.layout.fragment_photos, container, false);
+         
         return rootView;
     }
 }
