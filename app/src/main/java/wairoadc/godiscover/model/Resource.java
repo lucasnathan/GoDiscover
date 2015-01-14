37094210a0a52860d7ce61ea11a024363cbf3d85
Nathan (yudi) <lucas.nathan80@hotmail.com>
2015-01-14 14:52:54 +1300

    Revert "`Resources` change - Ian"
    This reverts commit 312269755f39125857aed4f2444e1a28da1841c5.

--- app/src/main/java/wairoadc/godiscover/model/Resource.java
+++ app/src/main/java/wairoadc/godiscover/model/Resource.java
@@ -9,8 +9,6 @@ import java.util.ArrayList;
 // Resource: Defines the structure for resources, which are the path for resources to be used by the app.
 public class Resource {
     private long id; // Id for the resource on the database.
-    private String name; // Name of the resource to be seen by the user.
-    private String story; // Story of that piece of resource.
     private String path; // Path of the resource on the application.
     private Type type; // Type of the resource.
 
@@ -22,22 +20,6 @@ public class Resource {
 
     public void setId(long id) {
         this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getStory() {
-        return story;
-    }
-
-    public void setStory(String story) {
-        this.story = story;
     }
 
     public String getResource() {
