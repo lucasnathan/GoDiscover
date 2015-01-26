f7240370af931b275d417a62efd27002baddd826
Nathan (yudi) <lucas.nathan80@hotmail.com>
2015-01-26 16:49:33 +1300

    26_01_2015_16_48

--- app/src/main/java/wairoadc/godiscover/view/activities/GalleryActivity.java
+++ app/src/main/java/wairoadc/godiscover/view/activities/GalleryActivity.java
@@ -30,7 +30,7 @@ public class GalleryActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_gallery, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
--- app/src/main/java/wairoadc/godiscover/view/activities/RedirectActivity.java
+++ app/src/main/java/wairoadc/godiscover/view/activities/RedirectActivity.java
@@ -30,7 +30,7 @@ public class RedirectActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_redirect, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
