8ff9f4868eef85c6817b29ee917597bc9b998bb9
Ian Nícolas Lemes Alves <ian.alves@outlook.com>
2015-08-23 22:00:24 -0300

    Gallery Captions Added
    The images in focus on the gallery now display a caption with
    information about that image.

--- app/src/main/java/wairoadc/godiscover/adapter/GalleryFocusAdapter.java
+++ app/src/main/java/wairoadc/godiscover/adapter/GalleryFocusAdapter.java
@@ -10,6 +10,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.support.v4.view.PagerAdapter;
@@ -19,28 +21,27 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import wairoadc.godiscover.R;
 import wairoadc.godiscover.services.BitmapWorkerTask;
+import wairoadc.godiscover.utilities.GradientOverImageDrawable;
 
 public class GalleryFocusAdapter extends PagerAdapter {
 
     private Activity activity;
     private List<String> imagePaths;
     private LayoutInflater inflater;
-<<<<<<< HEAD
-=======
     private List<String> imageStories;
->>>>>>> origin/master
     private static final String ADAPTER_TAG = "GalleryFocusAdapter";
 
     // constructor
     public GalleryFocusAdapter(Activity activity,List<String> imagePaths,List<String> imageInfo) {
         this.activity = activity;
-<<<<<<< HEAD
-=======
         this.imageStories = imageInfo;
->>>>>>> origin/master
         this.imagePaths = imagePaths;
     }
 
@@ -57,6 +58,7 @@ public class GalleryFocusAdapter extends PagerAdapter {
     @Override
     public Object instantiateItem(ViewGroup container, final int position) {
         ImageView imgDisplay;
+        TextView imgInfo;
 
         inflater = (LayoutInflater) activity
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -64,8 +66,10 @@ public class GalleryFocusAdapter extends PagerAdapter {
                 false);
 
         imgDisplay = (ImageView) viewLayout.findViewById(R.id.imgDisplay);
+        imgInfo = (TextView) viewLayout.findViewById(R.id.captionTextView);
         //Log.i("GalleryFocus","size: "+imgDisplay.getWidth()+" "+imgDisplay.getHeight());
         if(null != imagePaths.get(position)) {//unlocked spot show focus picture otherwise don't show
+            imgInfo.setText(imageStories.get(position));
             loadBitmap(activity.getFilesDir() + imagePaths.get(position), imgDisplay);
             imgDisplay.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -74,7 +78,13 @@ public class GalleryFocusAdapter extends PagerAdapter {
                 }
             });
         } else { //locked image show locked icon
-            imgDisplay.setImageResource(R.drawable.locked_resource);
+            imgInfo.setText("Go Discover to Unlock this Image");
+            int gradientStartColor = Color.argb(0, 0, 0, 0);
+            int gradientEndColor = Color.argb(255, 0, 0, 0);
+            Bitmap lockedBitmap = BitmapFactory.decodeResource(this.activity.getResources(), R.drawable.locked_resource);
+            GradientOverImageDrawable gradientDrawable = new GradientOverImageDrawable(this.activity.getResources(), lockedBitmap);
+            gradientDrawable.setGradientColors(gradientStartColor, gradientEndColor);
+            imgDisplay.setImageDrawable(gradientDrawable);
         }
         container.addView(viewLayout);
 
@@ -108,9 +118,13 @@ public class GalleryFocusAdapter extends PagerAdapter {
 
     public void loadBitmap(String currentImage, ImageView imageView) {
         final Bitmap bitmap = BitmapWorkerTask.getBitmapFromMemCache(currentImage);
+        int gradientStartColor = Color.argb(0, 0, 0, 0);
+        int gradientEndColor = Color.argb(255, 0, 0, 0);
         if(bitmap != null) {
             //Log.i(ADAPTER_TAG,"This image is in cache");
-            imageView.setImageBitmap(bitmap);
+            GradientOverImageDrawable gradientDrawable = new GradientOverImageDrawable(this.activity.getResources(), bitmap);
+            gradientDrawable.setGradientColors(gradientStartColor, gradientEndColor);
+            imageView.setImageDrawable(gradientDrawable);
         } else {
             if(cancelPotentialWork(currentImage,imageView)) {
                 final BitmapWorkerTask task = new BitmapWorkerTask(imageView);
@@ -136,6 +150,6 @@ public class GalleryFocusAdapter extends PagerAdapter {
 
     @Override
     public void destroyItem(ViewGroup container, int position, Object object) {
-        container.removeView((LinearLayout) object);
+        container.removeView((RelativeLayout) object);
     }
 }
